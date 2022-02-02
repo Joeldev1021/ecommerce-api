f2178e53afec8df4c22a1db1e68390cdc94e0bf0
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-02-02 10:33:11 -0500

    category model

--- src/models/category.js
+++ src/models/category.js
@@ -1,20 +1,11 @@
 const { Schema, model } = require("mongoose");
 
 const CategorySchema = new Schema({
-  name: {
-    type: String,
-    required: [true, "Name is required"]
-  },
-  state: {
-    type: Boolean,
-    default: true,
-    required: true
-  },
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: [true, "User is required"]
-  }
+    name: {
+        type: String,
+        required: [true, "Name is required"]
+    }
+
 });
 
 module.exports = model("Category", CategorySchema);
