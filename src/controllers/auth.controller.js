98d324b0dcdb525f7a677b0657f8b355207cc014
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-02-08 09:51:27 -0500

    encry password

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,21 +1,35 @@
+const { generateJWT } = require("../helpers/generateJWT");
 const UserSchema = require("../models/user");
 
 class AuthController {
     async signup (req, res) {
-        const userFind = await UserSchema.findOne({ email: req.body.email });
-        if (!userFind) return res.json({ message: `user already not exit ${userFind.email}` });
-        const user = new UserSchema(req.body);
-        user.password = await user.enCryptPassword(user.password);
-        await user.save();
-        res.json(user);
+        try {
+            const userFind = await UserSchema.findOne({ email: req.body.email });
+            if (userFind) return res.json({ message: `user already not exit ${userFind.email}` });
+            const user = new UserSchema(req.body);
+            user.password = await user.enCryptPassword(user.password);
+            const userSave = await user.save();
+            console.log(userSave);
+            res.json(userSave);
+        } catch (error) {
+            res.json({ message: error });
+        }
     };
 
     async signin (req, res) {
-        const user = await UserSchema.findOne({ email: req.body.email });
-        if (!user) return res.json({ message: "user not found" });
-        const result = await user.comparedPassword(req.body.password);
-        console.log(result);
-        res.json(user);
+        try {
+            const user = await UserSchema.findOne({ email: req.body.email });
+            if (!user) return res.json({ message: "user not found" });
+            const isPassword = await user.comparedPassword(req.body.password);
+            if (!isPassword) return res.json({ message: "password or email incorret" });
+            const jwt = await generateJWT(user.id);
+            res.json({
+                message: "signin success",
+                jwt
+            });
+        } catch (error) {
+            res.json({ message: error });
+        }
     };
 }
 
