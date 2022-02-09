8e20d45e730d17ec83d27e4aa321477ea87dc020
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-02-09 07:54:11 -0500

    extends time jwt

--- src/helpers/generateJWT.js
+++ src/helpers/generateJWT.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const generateJWT = (id) => {
-    const token = jwt.sign({ id }, process.env.SECRET_KEY_JWT, { expiresIn: 60 * 60 });
+    const token = jwt.sign({ id }, process.env.SECRET_KEY_JWT, { expiresIn: 24 * 60 * 60 });
     return token;
 };
 
