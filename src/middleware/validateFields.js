ff9f8968b6d7ab695da069c13489fbe2af33f8fe
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-02-09 07:55:39 -0500

    validate Jwt isAdmin

--- src/middleware/validateFields.js
+++ src/middleware/validateFields.js
@@ -1,12 +1,35 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 const { validationResult } = require("express-validator");
 
 const validatorFields = (req, res, next) => {
     const errors = validationResult(req);
+
     if (!errors.isEmpty()) {
         return res.status(400).json(errors);
     }
-
     next();
 };
 
-module.exports = validatorFields;
+const validateJWT = async (req, res, next) => {
+    const token = req.headers.authorization;
+    try {
+        const { id } = await jwt.verify(token, process.env.SECRET_KEY_JWT);
+        const user = await User.findById(id, { password: 0 });
+        req.user = user;
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: "Is not login" });
+    }
+};
+
+const isAdmin = (req, res, next) => {
+    const user = req.user;
+    if (user.role === "ADMIN_ROLE" || user.role === "SUPER_ADMIN_ROLE") {
+        next();
+    } else {
+        return res.status(401).json({ message: "not authorized" });
+    }
+};
+
+module.exports = { validatorFields, isAdmin, validateJWT };
--- src/middleware/validateRole.js
+++ src/middleware/validateRole.js
@@ -1,9 +1,6 @@
-const Role = require("../models/role");
 
 const validateRole = async (role = "") => {
-    const roleExists = await Role.findOne({ role });
-
-    if (!roleExists) {
+    if (!role) {
         throw new Error(`This role: ${role} does not exist`);
     }
 };
