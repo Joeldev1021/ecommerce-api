40378abc3fba1289c56644b79b45497813a7f546
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-02-28 20:35:41 -0500

    check user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,21 +1,41 @@
-
 const express = require("express");
+const { check } = require("express-validator");
 const { getAllUsers, getRoles, deleteUser, updateUser, getUserById } = require("../controllers/user.controller");
-const { isAdmin, validateJWT } = require("../middleware/validateFields");
+const { validUserByID } = require("../helpers");
+const { validatorFields, isAdmin } = require("../middleware/validateFields");
+const { validateJWT } = require("../middleware/validateJwt");
 
 const router = express.Router();
 
 router.get("/role", getRoles);
+
 router.get("/all", getAllUsers);
 
-router.get("/:id",
+router.get("/:id", [
+    check("id", "the ID is invalid").isMongoId(),
+    check("id").custom(validUserByID),
     validateJWT,
-    isAdmin,
-    getUserById
-)
-    .put("/:id", updateUser)
-    .delete("/:id",
+    validatorFields
+], getUserById);
 
-        deleteUser);
+router.put("/:id",
+    [
+        check("id", "the ID is invalid").isMongoId(),
+        check("id").custom(validUserByID),
+        validateJWT,
+        isAdmin,
+        validatorFields
+    ],
+    updateUser);
+
+router.delete("/:id",
+    [
+        check("id", "the ID is invalid").isMongoId(),
+        check("id").custom(validUserByID),
+        validateJWT,
+        isAdmin,
+        validatorFields
+    ],
+    deleteUser);
 
 module.exports = router;
