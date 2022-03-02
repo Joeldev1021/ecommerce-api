09be5dcf6a5576898a05978fa9d4ad1590b4db1d
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-03-01 20:07:41 -0500

    validateJwt

--- src/middleware/validateFields.js
+++ src/middleware/validateFields.js
@@ -1,5 +1,3 @@
-const jwt = require("jsonwebtoken");
-const User = require("../models/user");
 const { validationResult } = require("express-validator");
 
 const validatorFields = (req, res, next) => {
@@ -11,25 +9,13 @@ const validatorFields = (req, res, next) => {
     next();
 };
 
-const validateJWT = async (req, res, next) => {
-    const token = req.headers.authorization;
-    try {
-        const { id } = await jwt.verify(token, process.env.SECRET_KEY_JWT);
-        const user = await User.findById(id, { password: 0 });
-        req.user = user;
-        next();
-    } catch (error) {
-        return res.status(401).json({ message: "Is not login" });
-    }
-};
-
 const isAdmin = (req, res, next) => {
     const user = req.user;
-    if (user.role === "ADMIN_ROLE" || user.role === "SUPER_ADMIN_ROLE") {
+    if (user.role === "ADMIN_ROLE") {
         next();
     } else {
         return res.status(401).json({ message: "not authorized" });
     }
 };
 
-module.exports = { validatorFields, isAdmin, validateJWT };
+module.exports = { validatorFields, isAdmin };
