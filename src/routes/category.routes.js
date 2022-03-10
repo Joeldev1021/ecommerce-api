4344ec622c173a346e7c516cc5f876049fb51800
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-03-10 18:02:43 -0500

    validate fieldAuth

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -1,13 +1,41 @@
 const express = require("express");
+const { check } = require("express-validator");
 const router = express.Router();
 
 const CategoryController = require("../controllers/category.controllers");
-router.post("/", CategoryController.createCategory);
+const { validCategoryByID } = require("../helpers");
+const { validatorFields, validateJwt, isAdmin } = require("../middleware/validatetor");
 
 router.get("/", CategoryController.getAllCategory);
 
-router.get("/:id", CategoryController.getCategoryById)
-    .put("/:id", CategoryController.updateCategory)
-    .delete("/:id", CategoryController.deleteCategory);
+router.post("/", [
+    check("name", "the name is required").not().isEmpty(),
+    check("description", "the description is required").not().isEmpty(),
+    check("image", "the image is required").not().isEmpty(),
+    validatorFields
+], CategoryController.createCategory);
+
+router.get("/:id", [
+    check("id", "the ID is invalid").isMongoId(),
+    check("id").custom(validCategoryByID),
+    validateJwt,
+    validatorFields
+], CategoryController.getCategoryById);
+
+router.put("/:id", [
+    check("id", "the ID is invalid").isMongoId(),
+    check("id").custom(validCategoryByID),
+    validateJwt,
+    isAdmin,
+    validatorFields
+], CategoryController.updateCategory);
+
+router.delete("/:id", [
+    check("id", "the ID is invalid").isMongoId(),
+    check("id").custom(validCategoryByID),
+    validateJwt,
+    isAdmin,
+    validatorFields
+], CategoryController.deleteCategory);
 
 module.exports = router;
