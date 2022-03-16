06478f71c23cae6fa48448bbe018e95966955fc6
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-03-16 18:40:02 -0500

    delete get roles

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,12 +1,10 @@
-const User = require("../models/user");
-const Role = require("../models/role");
-
+const UserSchema = require("../models/user");
 // get user by id
 class UserController {
     async getUserById (req, res) {
         try {
             const { id } = req.params;
-            const user = await User.findById(id);
+            const user = await UserSchema.findById(id);
             res.json(user);
         } catch (error) {
             res.json({ message: error });
@@ -16,37 +14,34 @@ class UserController {
     // get all users
     async getAllUsers (req, res) {
         try {
-            const users = await User.find().select("-password");
+            const users = await UserSchema.find().select("-password");
             res.json(users);
         } catch (error) {
             res.json({ message: error });
         }
     };
 
+    // update user
     async updateUser (req, res) {
         try {
             const { id } = req.params;
-            const user = await User.findByIdAndUpdate(id, req.body);
+            const user = await UserSchema.findByIdAndUpdate(id, req.body);
             res.json(`${user.name} updated`);
         } catch (error) {
             res.json({ message: error });
         }
     };
 
+    // delete user
     async deleteUser (req, res) {
         try {
             const { id } = req.params;
-            const user = await User.findByIdAndDelete(id);
+            const user = await UserSchema.findByIdAndDelete(id);
             res.json(`${user.name} deleted`);
         } catch (error) {
             res.json({ message: error });
         }
     };
-
-    async getRoles (req, res) {
-        const roles = await Role.find();
-        res.json(roles);
-    }
 }
 
 module.exports = new UserController();
